fac06da6c89aa400bfbd47e197110dd2233ced28
Abhishek Ahirrao <ahirraoabhishek007@gmail.com>
2020-01-16 15:50:09 +0530

    added the normal calculators code in the bmi's code
    just adjusted the code of the calculator (copied didnt mess with the calc code)

--- BMI-calc/bmi_calc.js
+++ BMI-calc/bmi_calc.js
@@ -17,6 +17,18 @@ app.post('/', function(req,res){
   res.send("the result of the BMI calculator is" + result);
 });
 
+app.get('/index1',function( req, res) { //gets the request from browser to the '/' root and then invokes the function()
+  res.sendFile(__dirname + "/index1.html"); //can send html files with the res.sendFile method
+});
+
+app.post('/index1',function(req,res) {
+  var num1 = Number(req.body.n1); //gets the value of n1 from the html file and converts it into a number then stores in num1
+  var num2 = Number(req.body.n2);
+
+  var result = num1 + num2;
+  res.send("The result sum of the calculator is " + result);
+});
+
 app.listen(3000, function(){
   console.log("server started on server 3000");
 });
